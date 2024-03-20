421816c7dd05d2c0f8fac93eafb58953d8307ec2
Echo <91438643+echo-111@users.noreply.github.com>
2024-03-20 15:20:34 +1100

    Update dataApi.js

--- src/api/dataApi.js
+++ src/api/dataApi.js
@@ -1,30 +1,30 @@
-import axiosInstance from "./axiosInstance";
+// import axiosInstance from "./axiosInstance";
 
-class dataAPI {
-  uploadCallFlow = async(formData) => {
-    const response = await axiosInstance.post(`/data/callFlow/upload`, formData, {
-      headers: {
-        "content-type": "multipart/form-data"
-      }
-    }).then((res) => {
-      alert(JSON.stringify(res.data.data.data) + " [" + res.status + "]");
-      return res.data;
-    }).catch((err) => {
-      alert(err.response.data.message + " [" + err.response.status + "]");
-    });
-  };
-  uploadCommPeak = async(formData) => {
-    const response = await axiosInstance.post(`/data/commPeak/upload`, formData, {
-      headers: {
-        "content-type": ",multipart/form-data"
-      }
-    }).then((res) => {
-      alert(JSON.stringify(res.data.data.data) + " [" + res.status + "]");
-      return res.data;
-    }).catch((err) => {
-      alert(err.response.data.message + " [" + err.response.status + "]");
-    });
-  };
-}
+// class dataAPI {
+//   uploadCallFlow = async(formData) => {
+//     const response = await axiosInstance.post(`/data/callFlow/upload`, formData, {
+//       headers: {
+//         "content-type": "multipart/form-data"
+//       }
+//     }).then((res) => {
+//       alert(JSON.stringify(res.data.data.data) + " [" + res.status + "]");
+//       return res.data;
+//     }).catch((err) => {
+//       alert(err.response.data.message + " [" + err.response.status + "]");
+//     });
+//   };
+//   uploadCommPeak = async(formData) => {
+//     const response = await axiosInstance.post(`/data/commPeak/upload`, formData, {
+//       headers: {
+//         "content-type": ",multipart/form-data"
+//       }
+//     }).then((res) => {
+//       alert(JSON.stringify(res.data.data.data) + " [" + res.status + "]");
+//       return res.data;
+//     }).catch((err) => {
+//       alert(err.response.data.message + " [" + err.response.status + "]");
+//     });
+//   };
+// }
 
-export default new dataAPI();
+// export default new dataAPI();
